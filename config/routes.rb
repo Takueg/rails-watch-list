b7a2268aa575525d65e17787c131cc7a23f9d287
Takudzwa Gwindingwi <takueg@gmail.com>
2021-07-22 20:43:33 +0200

    fixing root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'bookmarks/new'
   get 'bookmarks/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :new, :create, :show] do
+  root to: 'lists#index'
+  resources :lists, only: [:new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
 
